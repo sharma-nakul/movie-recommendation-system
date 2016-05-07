890994bd8c28158163e9c2d0bbb100966b787bbb
Nakul Sharma <nakulsharma06@gmail.com>
2016-05-07 02:52:52 -0700

    movieCount property added

--- src/main/java/movie/model/User.java
+++ src/main/java/movie/model/User.java
@@ -2,17 +2,29 @@ package movie.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.io.Serializable;
+
 /**
  * Created by Naks on 02-May-16.
  * Read user who has logged in
  */
-public class User {
+public class User implements Serializable {
     private int userId;
     private String status;
+    private int movieCount;
 
     public User() {
         this.userId = 1;
         this.status="failure";
+        this.movieCount=0;
+    }
+
+    public int getMovieCount() {
+        return movieCount;
+    }
+
+    public void setMovieCount(int movieCount) {
+        this.movieCount = movieCount;
     }
 
     public String getStatus() {
