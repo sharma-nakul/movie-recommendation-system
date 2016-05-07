0eed337f6b7809d3e021a054d322ddff75e3636d
Nakul Sharma <nakulsharma06@gmail.com>
2016-05-07 02:49:42 -0700

    rename file name to history load

--- src/main/java/movie/operation/HistoryDataSet.java
+++ src/main/java/movie/operation/HistoryDataSet.java
@@ -13,23 +13,23 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 
 /**
- * Created by Shounak on 5/5/2016.
+ * Created by Shounak on 5/2/2016.
  * To get the data initially. The plan is to initialize the dataset once and then fetch from cassandra for all later queries
  */
-public class GetDataSet {
+public class HistoryDataSet {
     private JavaRDD<Movie> moviesRDD;
     private JavaRDD<Rating> ratingRDD;
     private JavaRDD<Tag> tagRDD;
     private JavaSparkContext jsc;
 
 
-    public GetDataSet(SparkContext sc) {
+    public HistoryDataSet(SparkContext sc) {
         this.jsc = JavaSparkContext.fromSparkContext(sc);
         this.moviesRDD = jsc.textFile(CONSTANT.getMoviesFilePath()).map(new MapMovieUDF());
         this.ratingRDD = jsc.textFile(CONSTANT.getRatingsFilePath()).map(new MapRatingUDF());
         this.tagRDD = jsc.textFile(CONSTANT.getTagsFilePath()).map(new MapTagUDF());
         DBService saveInCassandra = new DBService();
-        //Added by Shounak on 5th May 2016
+        //Added by Shounak on 2th May 2016
         saveInCassandra.saveMovies(this.moviesRDD); // Completed and Working
         saveInCassandra.saveRatings(this.ratingRDD); // Completed and Working
         saveInCassandra.saveTags(this.tagRDD); // Completed and Working
